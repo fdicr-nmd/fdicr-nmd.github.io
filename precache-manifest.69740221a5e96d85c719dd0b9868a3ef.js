1a3cdbb5b97f696b5b66347d06e1fd3e07fefb61
Andrés Díaz Soto <andres@estudiomanati.com>
2018-05-10 22:02:46 -0600

    Initial commit

---
+++ precache-manifest.69740221a5e96d85c719dd0b9868a3ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "97a77e3aa62b15ef5ace",
+    "url": "/js/vendor.97a77e3a.js"
+  },
+  {
+    "revision": "bb41d6d80c1fdaad6874",
+    "url": "/js/manifest.bb41d6d8.js"
+  },
+  {
+    "revision": "1a48c193516d42b51600",
+    "url": "/js/app.1a48c193.js"
+  },
+  {
+    "revision": "8f535d5573d0d7f510580074c43bf9b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c5be3423ff07bbf3618413245727ab6",
+    "url": "/img/social-icons.1c5be342.svg"
+  },
+  {
+    "revision": "582084eccf36ff4f5add14f8452df628",
+    "url": "/img/logo.582084ec.svg"
+  },
+  {
+    "revision": "918e228bcfec84de4cc062beffb46291",
+    "url": "/img/logo-color.918e228b.svg"
+  },
+  {
+    "revision": "a66b2b3cfabffd5b6e03be8fdd82296c",
+    "url": "/img/background-header.a66b2b3c.jpg"
+  },
+  {
+    "revision": "0cfdea65d40b26a00f9e68d1d67cf336",
+    "url": "/img/background-header-mobile.0cfdea65.jpg"
+  },
+  {
+    "revision": "1a48c193516d42b51600",
+    "url": "/css/app.607da26b.css"
+  }
+];
